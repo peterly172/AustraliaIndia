604ce01fe040a2ff0cf27a6c6768aeb4a77a7645
Peter Ly <peter.ly1@hotmail.com>
2020-12-14 22:48:21 +0000

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,60 @@
+CREATE TABLE match (
+id INT PRIMARY KEY,
+start_date DATE,
+end_date DATE,
+format_id INT,
+venue_id INT,
+teama_id INT NOT NULL,
+teamb_id INT NOT NULL,
+winner INT,
+potm INT 
+);
+
+
+CREATE TABLE formats (
+id SERIAL PRIMARY KEY,
+format VARCHAR(10)
+);
+
+CREATE TABLE teams (
+id SERIAL PRIMARY KEY, 
+name VARCHAR(50) NOT NULL,
+coach VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE venues (
+id SERIAL PRIMARY KEY,
+name VARCHAR(50),
+city VARCHAR(50)
+);
+
+CREATE TABLE roles (
+id SERIAL PRIMARY KEY NOT NULL,
+role VARCHAR(25) NOT NULL
+);
+
+
+CREATE TABLE wickets (
+id SERIAL PRIMARY KEY,
+dismissal VARCHAR(25)
+);
+
+CREATE TABLE players (
+id INT PRIMARY KEY NOT NULL,
+name VARCHAR(50) NOT NULL,
+role_id INT NOT NULL,
+team_id INT NOT NULL
+);
+
+CREATE TABLE matchruns(
+match_id INT NOT NULL,
+team_id INT NOT NULL,
+player_id INT,
+runs INT,
+balls INT,
+fours INT,
+sixes INT,
+bowler_id INT,
+wicket_id INT,
+notes VARCHAR(100)
+);
